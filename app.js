59457f36b432d1a5fe1d83a0bbf1bc113e6a41a1
jmc617 <jmc617@gmail.com>
2020-05-04 22:58:27 -0400

    express server started

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const PORT = 5000
+
+//req = request data 
+app.get("/", (req,res)=>{
+    res.send("hello world")
+});
+
+app.listen(PORT, ()=> {
+    console.log('server is running on', PORT)
+      
+})
